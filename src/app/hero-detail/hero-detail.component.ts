f7f68d8dba52fcb08dbc7529b1bfa557d79c7b73
tinyxiong <799497060@qq.com>
2018-12-24 17:30:15 +0800

    修改goBack方法

--- src/app/hero-detail/hero-detail.component.ts
+++ src/app/hero-detail/hero-detail.component.ts
@@ -28,7 +28,7 @@ export class HeroDetailComponent implements OnInit {
     this.heroService.getHero(id).subscribe(hero => this.hero = hero);
   }
 
-  goback(): void {
+  goBack(): void {
     this.location.back();
   }
 
